921848efcb0efa2453a2e08d0ec473291df41318
JVP3R3IRA <jvpereira351@gmail.com>
2025-05-24 23:14:05 +0000

    alteração do codigo para o bom funcionamento

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,19 +1,23 @@
-# include <stdio.h> // Inclui a biblioteca padrão de entrada e saída
+# include <stdio.h>  // Inclui a biblioteca padrão de entrada e saída
 
 int main () {
-    char estado;  // Armazena a letra do estado (A-H)
-    char codigo[4]; // Armazena o código da carta (ex: A01)
-    int  numero_da_carta;  // Armazena o número da carta (1 a 4)
-    char cidade[50];   // Armazena o nome da cidade
-    int populacao, ponto_turistico; // Armazena população e pontos turísticos
+    char estado, codigo[4], cidade[50];  // Armazena a letra do estado (A-H) //Armazena o código da carta (ex: A01) // Armazena o nome da cidade     
+    int populacao, ponto_turistico,  numero_da_carta; // Armazena população e pontos turísticos // Armazena o número da carta (1 a 4) 
     float area, pib;  // Armazena área e PIB da cidade
+
+// Variáveis para armazenar os dados da segunda cidade 
+
+    char estado2, codigo2[4], cidade2[50]; // Armazena a letra do estado (A-H)    Armazena o código da carta (ex: A01) e o nome da cidade
+    int numero_da_carta2, populacao2, ponto_turistico2; // Armazena o número da carta (1 a 4), população e pontos turísticos
+    float area2, pib2; // Armazena área e PIB da cidade        
+
 
 printf("Vamos realizar o cadastramento das Cidades\n\n"); // Mensagem inicial
  
 // Cadastro da primeira cidade
 
 printf("Digite uma letra de (A-H): ");
-scanf("%c", &estado); // Lê a letra do estado
+scanf(" %c", &estado); // Lê a letra do estado
 
 printf("Digite um numero de 1 a 4: ");
 scanf("%d", &numero_da_carta);  // Lê o número da carta
@@ -21,13 +25,12 @@ scanf("%d", &numero_da_carta);  // Lê o número da carta
 sprintf (codigo, "%c%02d", estado, numero_da_carta); // Monta o código da carta (ex: A01)
 
  printf("Digite o nome da cidade: "); 
- scanf("%s", &cidade); // Lê o nome da cidade
+ scanf("%s", cidade); // Lê o nome da cidade
 
  printf("Digite a populacão da cidade: "); 
  scanf("%d", &populacao); // Lê a população da cidade
 
  printf("Digite a área da cidade: ");
- scanf("%f", &area); // Lê a área da cidade
 
  printf("Digite o PIB da cidade: ");
  scanf("%f", &pib); // Lê o PIB da cidade
@@ -55,39 +58,39 @@ printf("A cidadde de %s tem %d pontos turisticos\n\n", cidade, ponto_turistico);
 printf("Vamos cadastrar mais cidades\n\n");
 
 printf("Digite uma letra de (A-H): ");
-scanf(" %c", &estado);
+scanf(" %c", &estado2);
 
-printf("Digite um numero de 1 a 4:");
-scanf("%d",  &numero_da_carta);
+printf("Digite um numero de 1 a 4: ");
+scanf("%d",  &numero_da_carta2);
+
+sprintf(codigo2, "%c%02d", estado2, numero_da_carta2);
 
 printf("Digite o nome da cidade: ");
-scanf("%s", cidade);
+scanf("%s", cidade2);
 
 printf("Digite a população da cidade: ");
-scanf("%d", &populacao);
+scanf("%d", &populacao2);
 
 printf("Digite a área da cidade: ");
-scanf("%f", &area);
+scanf("%f", &area2);
 
 printf("Digite o pib da cidade: ");
-scanf("%f", &pib);
+scanf("%f", &pib2);
 
 printf("Digite quantos pontos turisticos a cidade tem: ");
-scanf("%d", &ponto_turistico);
+scanf("%d", &ponto_turistico2);
 
-printf("Codigo da carta e: %s\n", codigo);
+printf("Codigo da carta e: %s\n", codigo2);
 
-printf("A cidade é: %s\n", cidade);
+printf("A cidade é: %s\n", cidade2);
 
-printf("A população é de %d habitantes\n", populacao);
+printf("A população é de %d habitantes\n", populacao2);
 
-printf("A área da cidade é de: %.2f km²\n", area);
+printf("A área da cidade é de: %.2f km²\n", area2);
 
-printf("O Pib da cidade de %s e de: %.3f Bilhoes de reais\n", cidade, pib);
+printf("O Pib da cidade de %s e de: %.3f Bilhoes de reais\n", cidade2, pib2);
 
-printf("A cidadde de %s tem %d pontos turisticos", cidade, ponto_turistico);
-
- 
+printf("A cidadde de %s tem %d pontos turisticos", cidade2, ponto_turistico2);
 
  return 0; // Indica que o programa terminou corretamente
 
